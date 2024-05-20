c742a0b6053be63d1938aaa785a63e26628c7c02
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-05-20 18:00:35 +0500

    jira-clone

--- app.vue
+++ app.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import {styles} from '@/util/style'
+</script>
+
+
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline bg-black text-asosiy md:text-red-600 font-montserrat">
+  <!-- <h1 class="text-3xl font-bold underline bg-black text-asosiy md:text-red-600 font-montserrat">
       Hello world!
-    </h1>
+    </h1> -->
+  <div class="bg-asosiy w-full overflow-hidden">
+    
+    <div :class="[styles.paddingX]" class="py-5">
+      <p class="text-white">Navbar</p>
+    </div>
+
+    <div></div>
+
   </div>
 </template>
