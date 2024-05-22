eb23e9e83ada8374bf653d7f065225e7082320aa
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-05-22 17:16:43 +0500

    jira-clone

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import {styles} from '@/util/style'
+import {styles} from '@/util/style';
+
+console.log(styles); 
+
 </script>
 
 
@@ -7,13 +10,11 @@ import {styles} from '@/util/style'
   <!-- <h1 class="text-3xl font-bold underline bg-black text-asosiy md:text-red-600 font-montserrat">
       Hello world!
     </h1> -->
-  <div class="bg-asosiy w-full overflow-hidden">
+  <div class="bg-asosiy w-full overflow-hidden font-montserrat">
     
-    <div :class="[styles.paddingX]" class="py-5">
+    <div :class="[styles.padding]">
       <p class="text-white">Navbar</p>
     </div>
-
-    <div></div>
 
   </div>
 </template>
--- util/style.js
+++ util/style.js
@@ -1,3 +1,9 @@
 export const styles = {
-    paddingX: 'sm:px-16 px-6'
+    padding: "sm:px-16 px-6 sm:py-12 py-4",
+    paddingX: "sm:px-16 px-6",
+    paddingY: "sm:py-16 py-6",
+    
+    margin: "sm:mx-16 mx-6 sm:my-12 my-4",
+    marginX: "sm:mx-16 mx-6",
+    marginY: "sm:my-16 my-6",
 }
