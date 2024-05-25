298704299003331a16541ab9a9f24709c18ebab1
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-05-25 10:30:45 +0500

    jira-clone

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { styles } from '@/util/style';
+// import { styles } from '@/util/style';
 </script>
 
 
@@ -7,17 +7,17 @@ import { styles } from '@/util/style';
   <!-- <h1 class="text-3xl font-bold underline bg-black text-asosiy md:text-red-600 font-montserrat">
       Hello world!
     </h1> -->
-  <div class="bg-asosiy w-full overflow-hidden font-montserrat container">
+  <div class="bg-asosiy w-full overflow-hidden font-montserrat">
     
-    <div :class="[styles.padding, 'container']">
+    <div class="sm:px-16 px-6 sm:py-12 py-4 xl:max-w-[1280px] w-full flex justify-start">
       <!-- <div class="container"> -->
-      <div :class="[styles.container, 'container', styles.flexCenter]">
+      <div class="item-center">
         <p class="xs:text-rose-600 md:text-white">Navbar</p>
       </div>
     </div>
 
-    <div class="bg-slate-500 container ">
-      <p :class="[styles.paragraph]">
+    <div class="bg-slate-500">
+      <p class="font-montserrat font-normal text-oq text-[18px] leading-[30.8px] p-10">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora fugit numquam repudiandae accusantium, temporibus voluptatum nostrum quis ducimus! Pariatur quam, qui labore rem omnis consectetur totam accusamus alias excepturi. Ullam?
       </p>
     </div>
